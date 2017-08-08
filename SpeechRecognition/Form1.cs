a4632052448c2c453b9dbb9d259a7940e93fce38
nick wise <nickmwise@gmail.com>
2017-08-08 14:55:43 -0700

    add basic form to take in audio input from default audio source and allow the user to say, say hello as a command and have it return a messagebox

---
+++ SpeechRecognition/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+
+
+namespace SpeechRecognition
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);btnDisable.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices(); 
+            commands.Add(new string[] { "say hello", "print my name" });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammer = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammer);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+        }
+
+        private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "say hello":
+                    MessageBox.Show("Hello Nick. How are you?");
+                    break;
+                case "print my name":
+                    richTextBox1.Text += "\nNick";
+                    break;
+            }
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false; 
+        }
+    }
+}
